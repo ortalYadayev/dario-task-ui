462b10fe8c5d8252ec4c326eee435f7ca2d69cb1
Ortal Yadayev <ortalll20111@gmail.com>
2022-04-12 10:51:53 +0300

    init

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+// tailwind.config.js
+module.exports = {
+	purge: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
+	mode: 'jit',
+	darkMode: false,
+	theme: {
+		extend: {},
+	},
+	plugins: [],
+}
